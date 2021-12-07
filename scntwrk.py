a98b6c1c2b0893fd189c69bcba76d8b8b938fb57
alisrc88 <alisarac@live.com>
2021-12-07 02:04:51 +0100

    Added feature to control if nmap is installed

--- scntwrk.py
+++ scntwrk.py
@@ -18,14 +18,20 @@ dist = distro.name()
 
 # Check if system is Linux or MacOS
 if platform == "Linux" or platform == "linux2":
-    os.system("sudo pacman -S nmap")
+    # Control if nmap is installed
+    notFound = os.popen('which nmap').read()
+    if not notFound:
+        os.system("sudo pacman -S nmap")
     os.system("clear")
     print("Your operating system is Linux " + dist)
     os.system("sudo nmap -sn " + local_ip +"/24")
 elif platform == "Darwin":
-    os.system("brew install nmap")
+    # Control if nmap is installed
+    notFound = os.popen('which nmap').read()
+    if not notFound:
+        os.system("brew install nmap")
     os.system("clear")
-    print("Your ip-address is: " + local_ip)
+    print("You have nmap Your ip-address is: " + local_ip)
     print("Your operating system is MacOS " + dist)
     os.system("sudo nmap -sn " + local_ip +"/24")
 
